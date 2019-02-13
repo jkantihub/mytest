ba3b5f924593b5834f5cde5e28f23a5d8281e0dd
jkantihub <jkantihub@users.noreply.github.com>
2019-02-12 18:07:25 -0800

    Update string

--- message.php
+++ message.php
@@ -1,14 +1,5 @@
 <?php
 print("Hello World");
 
-print "print() also works without parentheses.";
-
-print "This spans
-multiple lines. The newlines will be
-output as well";
-
-print "This spans\nmultiple lines. The newlines will be\noutput as well.";
-
-print "escaping characters is done \"Like this\".";
-
+echo '<time datetime="'.date('c').'">'.date('Y - m - d').'</time>';
 ?>
