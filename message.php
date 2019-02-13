c4347147dfd4d9c456f3efbeb088e8ff9be556ca
jkantihub <jkantihub@users.noreply.github.com>
2019-02-12 17:43:51 -0800

    Added message.php

---
+++ message.php
@@ -0,0 +1,14 @@
+<?php
+print("Hello World");
+
+print "print() also works without parentheses.";
+
+print "This spans
+multiple lines. The newlines will be
+output as well";
+
+print "This spans\nmultiple lines. The newlines will be\noutput as well.";
+
+print "escaping characters is done \"Like this\".";
+
+?>
